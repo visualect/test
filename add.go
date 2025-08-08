e46df9b932956625e736a9afc2c1ab040563c1be
ian <yanikidev@gmail.com>
2025-08-08 13:12:01 +0200

    add go doc

--- add.go
+++ add.go
@@ -1,5 +1,11 @@
+// Add package has a function for adding two intergers
 package add
 
+// Add function takes two integers and returns the sum of them
+//
+// See [Addition]
+//
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
