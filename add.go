751865fedb814c450149520c19c23b88fa995def
ian <yanikidev@gmail.com>
2025-08-08 13:18:39 +0200

    make add func generic

--- add.go
+++ add.go
@@ -1,11 +1,16 @@
 // Add package has a function for adding two intergers
 package add
 
+type Number interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~float32 | ~float64
+}
+
 // Add function takes two integers and returns the sum of them
 //
 // See [Addition]
 //
 // [Addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
